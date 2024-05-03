bd5792809cb0a3c45b8b5356cb80bdf4d58634fb
Rakeshreddy7893 <rakeshreddykalagiri@gmail.com>
2024-05-03 23:41:15 +0530

    Done With CRUD using EJS Templating

--- WanderLust/app.js
+++ WanderLust/app.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listing")
 const path = require('path');
+const methodOverride = require('method-override')
 
 
 app.set('view engine',"ejs");
 app.set("views",path.join(__dirname,"views"));
-
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"))
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
 main().then((res)=>{
@@ -28,6 +30,43 @@ console.log("server is listening to port:8080");
 app.get('/listings',async(req,res)=>{
   const allListings = await Listing.find({});
   res.render("listings/index.ejs",{allListings});
+})
+
+
+app.get("/listings/new",(req,res)=>{
+  res.render("listings/new.ejs")
+})
+
+app.get('/listings/:id',async(req,res)=>{
+   let{id} = req.params;
+   const listing =await Listing.findById(id)
+   res.render("listings/show.ejs",{listing})
+})
+
+
+app.post('/listings',async(req,res)=>{
+const newListing =new Listing (req.body.listing)
+await newListing.save();
+res.redirect('/listings')
+})
+
+app.get('/listings/:id/edit',async(req,res)=>{
+  let{id} = req.params;
+   const listing =await Listing.findById(id)
+   res.render("listings/edit.ejs",{listing})
+})
+
+app.put('/listings/:id',async(req,res)=>{
+  let{id} = req.params;
+   await Listing.findByIdAndUpdate(id,{...req.body.listing})
+   res.redirect(`/listings/${id}`);
+})
+
+app.delete('/listings/:id',async(req,res)=>{
+  let{id} = req.params;
+  let deletedListing=await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
 })
 
 // app.get("/testListing",async(req,res)=>{
