b6c0bf4c1fa7ad2777a34bb2672f416d38864acc
Rakeshreddy7893 <rakeshreddykalagiri@gmail.com>
2024-05-20 13:04:10 +0530

    Added login and SignUp functionalty using passport

--- WanderLust/app.js
+++ WanderLust/app.js
@@ -7,10 +7,15 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js')
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
 
-const listings = require('./routes/listing.js')
-const reviews = require('./routes/review.js')
 
+
+const listingsRouter = require('./routes/listing.js')
+const reviewsRouter = require('./routes/review.js')
+const userRouter = require('./routes/user.js')
 
 app.set('view engine',"ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -50,14 +55,34 @@ app.get('/',(req,res)=>{
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req,res,next)=>{
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 })
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews)
+// app.get('/demouser',async(req,res)=>{
+//   let fakeUser = new User({
+//     email:"student@gmail.com",
+//     username:"delta-student"
+//   });
+//   let registredUser = await User.register(fakeUser,'helloworld');
+//   res.send(registredUser);
+// })
+
+app.use("/listings",listingsRouter);
+app.use("/listings/:id/reviews",reviewsRouter);
+app.use('/',userRouter);
 
 app.listen(8080,()=>{
 console.log("server is listening to port:8080");
