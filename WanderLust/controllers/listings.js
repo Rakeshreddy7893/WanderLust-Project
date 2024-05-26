2dd201dd6bb2e67c0675c720f242e215cd320865
Rakeshreddy7893 <rakeshreddykalagiri@gmail.com>
2024-05-26 14:50:21 +0530

    Integrated Maps using MapBox API

--- WanderLust/controllers/listings.js
+++ WanderLust/controllers/listings.js
@@ -1,7 +1,7 @@
 const Listing = require('../models/listing');
-
-
-
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 module.exports.index = async(req,res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings});
@@ -23,13 +23,22 @@ module.exports.showListing =async(req,res)=>{
 
 
 module.exports.createListing =async(req,res,next)=>{
+   let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1
+      })
+    .send()
+      
     let url = req.file.path;
     let filename = req.file.filename;
-    
     const newListing =new Listing (req.body.listing)  
     newListing.owner = req.user._id
     newListing.image ={url,filename}
-    await newListing.save();
+
+    newListing.geometry = response.body.features[0].geometry;
+
+   let savedListing = await newListing.save();
+   console.log(savedListing)
     req.flash("success","New Listing Created");
     res.redirect('/listings') 
 }
